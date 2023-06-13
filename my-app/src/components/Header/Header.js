43d056f86479a78a0940115a81d5f7954a8e29eb
Rashid Baghirov <rashidtb@code.edu.az>
2023-06-14 00:52:05 +0400

    Step By Step High Honor

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,8 +1,18 @@
-import React from "react";
-import "./_Header.scss";
-import { Link } from "react-router-dom";
-
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import './_Header.scss';
+import { Link } from 'react-router-dom';
 function Header() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <header className="header">
       <div className="container-fluid">
@@ -10,13 +20,12 @@ function Header() {
           <div className="col-lg-4 d-flex justify-content-center align-items-center">
             <div className="address">
               <p>
-                Nizami küçəsi 203B, AF Business House, 2-ci mərtəbə (+994 12)
-                310 0113
+                Nizami küçəsi 203B, AF Business House, 2-ci mərtəbə (+994 12) 310 0113
               </p>
             </div>
           </div>
           <div className="col-lg-4 d-flex justify-content-center align-items-center">
-            <div >
+            <div>
               <div className="social-icons">
                 <a className="m-3">
                   <i className="bi bi-facebook"></i>
@@ -35,9 +44,9 @@ function Header() {
           </div>
           <div className="col-lg-4">
             <div className="row">
-              <div className="col-lg-6  d-flex justify-content-center align-items-center">
+              <div className="col-lg-6 d-flex justify-content-center align-items-center">
                 <div className="searching">
-                  <a className="m-3">
+                  <a className="m-3" onClick={handleModalOpen}>
                     <i className="bi bi-search"></i>
                   </a>
                 </div>
@@ -55,6 +64,16 @@ function Header() {
           </div>
         </div>
       </div>
+      <Modal show={showModal} onHide={handleModalClose}  className="custom-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <img src="images/logofoot.png" alt="CodeAcademy Logo" className="modal-logo" />
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input placeholder="Axtar" style={{ boxShadow: 'none' }} className="form-control search_input" type="search" />
+        </Modal.Body>
+      </Modal>
     </header>
   );
 }
